3fb05de3a9cafb6e1b2a82d8af5ad7acce38324d
0nePoint0ne <40418659+0nePoint0ne@users.noreply.github.com>
2018-09-15 03:15:12 +0900

    new parse method using exist library
    works much better

--- WeatherAnalyzer.py
+++ WeatherAnalyzer.py
@@ -1,4 +1,6 @@
 import re
+import xml.etree.ElementTree as ET
+
 class dailyWeather:
 	def __init__(self, day, month, year, maxTemp, minTemp, meanTemp, heatDegDays, coolDegDays, totalRain, totalSnow, totalPrecipitation, snowOnGround):
 		self.day = day
@@ -13,46 +15,25 @@ class dailyWeather:
 		self.totalSnow = totalSnow
 		self.totalPrecipitation = totalPrecipitation
 		self.snowOnGround = snowOnGround
-	def displayData():
-		print("")
+		#self.displayData()
+	def displayData(self):
+		print("The Date is " + str(self.year) + "/" + str(self.month) + "/" + str(self.day) + "  Max Temp: " + str(self.maxTemp) + " Min Temp: " + str(self.minTemp) )
 		
 def main():
-	status = 0
-	da = ''
-	file = open("eng-daily-01012017-12312017.xml", "r")
-	data = file.read()
-	text = data.split('<stationdata ')
-	#print(file.read())
+	#file = open("data.xml", "r")
 	yearsWeather = []
 	#for i in range(365):
-		#yearsWeather.append(dailyWeather(1, 2, 3, 0,2,3,4,5,6,7,8,9,10,11))
-	for ob in text[1:2]:
-		print(" ")
-		#print(ob)
-		print(" ")
-		newText = ''
-		x = 0
-		for x in range(len(ob)):
-			
-			if(status == 1):
-				if(ob[x] == '>' or ob[x] == '<' or ob[x] == '"'):
-					status = 0
-				elif(ob[x] != '>' or ob[x] != '<' or ob[x] != '"'):
-					newText += ob[x]
-			elif(status == 0):
-				if(ob[x] == '>' or ob[x] == '<' or ob[x] == '"'):
-					status = 1
-					newText += " "
+		#
 		
-		da = newText.split(" ")
-		yearsWeather.append(dailyWeather(da[1], da[2], da[3], da[8],da[13],da[18],da[24],da[30],da[35],da[40],da[45],da[51]))
+	tree = ET.parse('data.xml')
+	root = tree.getroot()
+	for child in root:
+		if(str(child.tag) == 'stationdata'):
+			#print( str(child.tag) + " " + str(child.attrib) +  " " + str(child.attrib['day']))
+			yearsWeather.append(dailyWeather(child.attrib['day'], child.attrib['month'], child.attrib['year'], child[0].text,child[1].text,child[2].text,child[3].text,child[4].text,child[5].text,child[6].text,child[7].text,child[8].text))
+	
+	for day in yearsWeather:
+		day.displayData()
 
-		#for a in da:
-			#print(a)
-			#print(ob.find("Maximum Temperature"))
-	#	print(newText)
-
-		#for o in yearsWeather:
-			#print(o.day)
-		print(" ")
+	#file.close() 
 main()
