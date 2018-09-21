8efdd32d18d6b6a7cc4fee13b1e735d5ce0bd8ae
0nePoint0ne <40418659+0nePoint0ne@users.noreply.github.com>
2018-09-21 23:12:56 +0900

    need to fix
    something broken

--- WeatherAnalyzer.py
+++ WeatherAnalyzer.py
@@ -1,34 +1,122 @@
+import pandas as pd
 import re
 import xml.etree.ElementTree as ET
 
-class dailyWeather:
-	def __init__(self, day, month, year, maxTemp, minTemp, meanTemp, heatDegDays, coolDegDays, totalRain, totalSnow, totalPrecipitation, snowOnGround):
-		self.day = day
-		self.month = month
+dataset = []
+index = []
+
+class monthWeather:
+	def __init__(self, month, year, maxTemp, minTemp, meanTemp, heatDegDays, coolDegDays, totalRain, totalSnow, totalPrecipitation, snowOnGround):
+
+		if(int(month) < 10):
+			self.month = "0" + str(month)
+		else:
+			self.month = str(month)
+		
+		#self.month = month
 		self.year = year
 		self.maxTemp = maxTemp
 		self.minTemp = minTemp
 		self.meanTemp = meanTemp
 		self.heatDegDays = heatDegDays
 		self.coolDegDays = coolDegDays
-		self.totalRain = totalRain
-		self.totalSnow = totalSnow
-		self.totalPrecipitation = totalPrecipitation
-		self.snowOnGround = snowOnGround
+		if(totalRain == None):
+			self.totalRain = 0.0
+		else:
+			self.totalRain = totalRain
+		if(totalSnow == None):
+			self.totalSnow = 0.0
+		else:
+			self.totalSnow = totalSnow
+		if(totalPrecipitation == None):
+			self.totalPrecipitation = 0.0
+		else:
+			self.totalPrecipitation = totalPrecipitation
+		if(snowOnGround == None):
+			self.snowOnGround = 0
+		else:
+			self.snowOnGround = snowOnGround
+		
 		#self.displayData()
 	def displayData(self):
-		data = ''
+		strdata = ''
 		data = "The Date is " + str(self.year) + "/" + str(self.month) + "/" + str(self.day) + "  Max Temp: " + str(self.maxTemp) + " Min Temp: " + str(self.minTemp) 
-		return data
-		
-def printData(args):
-	file = open("data.txt", "w")
-	for day in args:
-		data = day.displayData()
-		file.write(data + "\n")
-	file.close
+		return strdata
 
+class dailyWeather:
+	def __init__(self, day, month, year, maxTemp, minTemp, meanTemp, heatDegDays, coolDegDays, totalRain, totalSnow, totalPrecipitation, snowOnGround):
+		if(int(day) < 10):
+			self.day = "0" + str(day)
+		else:
+			self.day = str(day)
+			
+		#self.day = day
+		if(int(month) < 10):
+			self.month = "0" + str(month)
+		else:
+			self.month = str(month)
 		
+		#self.month = month
+		self.year = year
+		self.maxTemp = maxTemp
+		self.minTemp = minTemp
+		self.meanTemp = meanTemp
+		self.heatDegDays = heatDegDays
+		self.coolDegDays = coolDegDays
+		if(totalRain == None):
+			self.totalRain = 0.0
+		else:
+			self.totalRain = totalRain
+		if(totalSnow == None):
+			self.totalSnow = 0.0
+		else:
+			self.totalSnow = totalSnow
+		if(totalPrecipitation == None):
+			self.totalPrecipitation = 0.0
+		else:
+			self.totalPrecipitation = totalPrecipitation
+		if(snowOnGround == None):
+			self.snowOnGround = 0
+		else:
+			self.snowOnGround = snowOnGround
+		
+		#self.displayData()
+	def displayData(self):
+		strdata = ''
+		data = "The Date is " + str(self.year) + "/" + str(self.month) + "/" + str(self.day) + "  Max Temp: " + str(self.maxTemp) + " Min Temp: " + str(self.minTemp) 
+		return strdata
+		
+def storeData(year):
+	file = open("data.text", "w")
+
+	for day in year:
+		
+		dataset.append('TimeStap:': str(day.year + "-" + day.month + "-" + day.day) ,'Max Temp:': float(day.maxTemp),'Min Temp:': float(day.minTemp), 'Mean Temp:': day.meanTemp, 'Total Rain:': day.totalRain, 'Total Snow:': day.totalSnow, 'Total Percipitation:': day.totalPrecipitation, 'Snow On Ground:': day.snowOnGround})
+	
+	#print(dataset[2])
+	df = pd.DataFrame(dataset)
+
+	df.to_csv("data.csv")
+	print(df.to_string())
+	file.write(str(df.to_string()))
+	file.close
+	
+def monthlySummary(year):
+	temp = {}
+	
+	file = open("dataSummary.txt","w")
+	value = -1
+	for data in dataset:
+		temp = data
+		value = value + 1
+		
+		#print(index[value] + " " + str(temp['Max Temp:']))
+	#print(dataset)
+	#print(dataset['Max Temp:'])
+ 
+	#file.write(str(df))
+	file.close
+	
 def main():
 	yearsWeather = []
 
@@ -41,6 +129,8 @@ def main():
 	
 	for day in yearsWeather:
 		day.displayData()
-		
-	printData(yearsWeather)
+	storeData(yearsWeather)
+	monthlySummary(yearsWeather)
+	
+	
 main()
